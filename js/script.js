d195b4405567814a2b3b198f6911742890ca5183
Alexander Brett <alexander.brett@sophos.com>
2014-08-26 18:10:35 +0100

    Refactor page structure

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+$(".expanded").removeClass("expanded");
+
 $(".blog-post").click(function(e){
   if(!$(e.target).is(".expander")) $(this).addClass("expanded");
 });
@@ -6,3 +8,8 @@ $(".expander").click(function(e){
   $(this).parent().toggleClass("expanded");
 });
 
+$(".gallery-item").click(function(e){
+  var background = $(this).css("background-image").replace(/.*\s?url\([\'\"]?/, '').replace(/[\'\"]?\).*/, '');
+  $("body").append(
+  )
+});

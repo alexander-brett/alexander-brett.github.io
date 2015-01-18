6bc4f24e897dedd0d3f8617f1eac8eaa10aa33ab
alexander-brett <alex@alexander-brett.co.uk>
2015-01-18 09:56:16 +0000

    more tweaking to the async page loading

--- js/script.js
+++ js/script.js
@@ -1,70 +1,68 @@
 function highlightLinks(){
-  $("a").each(function(){
-    var href = window.location.pathname == '/' ? '/index.html' : window.location.pathname;
-    if ($(this).attr("href") == href) $(this).addClass('activeLink');
-    else $(this).removeClass('activeLink');
-  })
-}
+    $("a").each(function(){
+	var href = window.location.pathname == '/' ? '/index.html' : window.location.pathname;
+	if ($(this).attr("href") == href) $(this).addClass('activeLink');
+	else $(this).removeClass('activeLink');
+    })
+	}
 
 function transitionContent(newDiv){
-    var $oldDiv = $($("#content").children());
+    var $oldDiv = $("#content").children();
     var $newDiv = $(newDiv).hide();
     
     $oldDiv.fadeOut(function(){
-      $oldDiv.remove();
-      $newDiv.fadeIn();
+	$oldDiv.remove();
+	$("#content").append($newDiv);
+	$newDiv.fadeIn();
+	highlightLinks();
+	setupLinks();
     });
     
     
-    $("#content").append($newDiv);
 }
 
 function navigateToLink(href, callback){
-  $.ajax(href).done(function(data){
-    var newDiv;
-    if(href.match(/.png|.jpg|.webm|.jpeg/)) {
-      newDiv = $('<div><a class="back internal" href="' + window.location.pathname + '">&#9664;</a></div>')
-        .addClass('gallery-expanded')
-        .css({"background-image": "url("+href+")"});
-    }  else {
-	newDiv = $(
-	    $(data).filter(function(i,e){return e.id=="content"})
-	)[0];
-    }
-    transitionContent(newDiv);
-    if (callback) callback();
-    highlightLinks();
-    setupLinks();
-  });
+    $.ajax(href).done(function(data){
+	var newDiv;
+	if(href.match(/.png|.jpg|.webm|.jpeg/)) {
+	    newDiv = $('<div><a class="back internal" href="' + window.location.pathname + '">&#9664;</a></div>')
+		.addClass('gallery-expanded')
+		.css({"background-image": "url("+href+")"});
+	}  else {
+	    newDiv = $(data).filter(function(i,e){return e.id=="content"}).children();
+	}
+	transitionContent(newDiv);
+	if (callback) callback();
+    });
 }
 
 function setupLinks(){
-  $(".expanded:not(.noShrink)").removeClass("expanded");
-  
-  $(".blog-post").off().click(function(e){
-    if(!$(e.target).is(".expander")) $(this).addClass("expanded");
-  });
-  
-  $(".expander").off().click(function(e){
-    $(this).parent().toggleClass("expanded");
-  });
-  
-  $("a.internal:not(.activeLink)").off().click(function(e){
-    var href = $(this).attr('href');
-    navigateToLink(href, function(){history.pushState(null, null, href)});
-    e.preventDefault();
-  });
-  
-  $("a.activeLink").off().click(function(e){
-    e.preventDefault();
-  });
+    $(".expanded:not(.noShrink)").removeClass("expanded");
+    
+    $(".blog-post").off().click(function(e){
+	if(!$(e.target).is(".expander")) $(this).addClass("expanded");
+    });
+    
+    $(".expander").off().click(function(e){
+	$(this).parent().toggleClass("expanded");
+    });
+    
+    $("a.internal:not(.activeLink)").off().click(function(e){
+	var href = $(this).attr('href');
+	navigateToLink(href, function(){history.pushState(null, null, href)});
+	e.preventDefault();
+    });
+    
+    $("a.activeLink").off().click(function(e){
+	e.preventDefault();
+    });
 }
 
 $(window).on("popstate", function(e){
-  navigateToLink(location.href);
+    navigateToLink(location.href);
 });
 
 $(function(){
-  highlightLinks();
-  setupLinks();
+    highlightLinks();
+    setupLinks();
 });

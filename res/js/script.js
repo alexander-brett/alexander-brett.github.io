ca9aad12fdfae53b7cf27ee3d891f7f8b0a22811
Alexander Brett <alex@alexander-brett.co.uk>
2016-06-01 22:03:57 +0100

    Scrolly header thing to make Lise happy

--- res/js/script.js
+++ res/js/script.js
@@ -4,59 +4,70 @@ function highlightLinks(){
     );});
 }
 
+function setupHeaderScrolling(){
+  $('#blog').scroll(function(e){
+    var header = $('header');
+    var body = $('#blog');
+    var offset = body.scrollTop();
+    var top = Math.min(offset, $('header h1').outerHeight(true));
+    header.css('top', -top);
+  });
+}
+
 function transitionContent(newDiv){
     var $oldDiv = $("#content").children();
     var $newDiv = $(newDiv).hide();
-    
+
     $oldDiv.fadeOut(function(){
         $oldDiv.remove();
         $("#content").append($newDiv.fadeIn());
         highlightLinks();
         setupLinks();
+        setupHeaderScrolling();
     });
 }
 
 function navigateToLink(href, callback){
     $.ajax(href).done(function(data){
         var newDiv;
-	if (href.match(/.png|.jpg|.webm|.jpeg/)) {
-	    newDiv =  $(
-		'<div><a class="back internal" href="'
-		    + window.location.pathname + '">&#9664;</a></div>'
-	    ).addClass(
-		'gallery-expanded'
-	    ).css({
-		"background-image": "url("+href+")"
-	    })
-	} else {
-	    newDiv = $(data).filter(
-		function(i,e){return e.id=="content"}
-	    ).children();
-	    document.title = $(data).filter('title').text();
-	}
-	
-	transitionContent(newDiv);
-	if (callback) callback();
+      	if (href.match(/.png|.jpg|.webm|.jpeg/)) {
+      	    newDiv =  $(
+      		'<div><a class="back internal" href="'
+      		    + window.location.pathname + '">&#9664;</a></div>'
+      	    ).addClass(
+      		'gallery-expanded'
+      	    ).css({
+      		"background-image": "url("+href+")"
+      	    })
+      	} else {
+      	    newDiv = $(data).filter(
+      		function(i,e){return e.id=="content"}
+      	    ).children();
+      	    document.title = $(data).filter('title').text();
+      	}
+
+      	transitionContent(newDiv);
+      	if (callback) callback();
     });
 }
 
 function setupLinks(){
     $(".expanded:not(.noShrink)").removeClass("expanded");
-    
+
     $(".blog-post").off("click").click(function(e){
         if(!$(e.target).is(".expander")) $(this).addClass("expanded");
     });
-    
+
     $(".expander").off("click").click(function(e){
         $(this).parent().toggleClass("expanded");
     });
-    
+
     $("a.internal:not(.activeLink)").off("click").click(function(e){
         var href = $(this).attr('href');
         navigateToLink(href, function(){history.pushState(null, null, href)});
         e.preventDefault();
     });
-    
+
     $("a.activeLink").off("click").click(function(e){
         e.preventDefault();
     });
@@ -67,6 +78,7 @@ $(window).on("popstate", function(e){
 });
 
 $(function(){
+    setupHeaderScrolling();
     highlightLinks();
     setupLinks();
 });
